42d6bae0a51714743863087fe5b17d2fa5e011f2
Dimeji1267 <salawudimeji1@gmail.com>
2022-09-09 06:24:33 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{	
+	puts("\"Programming is like bilding a multilingual puzzle");
+	Return (0);
+}	
+
