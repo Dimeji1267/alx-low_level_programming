88acebeb92c75661763efd84b2ff975145a1086a
Dimeji267 <salawudimeji1@gmail>
2022-09-10 15:12:39 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Description: using the main function
  *
- * this program prints "Programming is like building a multilingual puzzle"
+ * this program prints ("\"Programming is like building a multilingual puzzle")
  *
  * return (0)
  */
