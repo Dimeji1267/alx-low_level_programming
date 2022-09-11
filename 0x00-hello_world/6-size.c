c22f3772c9efee8424f24474421e63aa745aa3d0
Dimeji267 <salawudimeji1@gmail>
2022-09-11 09:18:57 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - print the string in the put function
  *
- * Return: Always 0 (Success)
+ * Description: using the main function
+ *
+ * this program prints "Programming is like building a multilingual puzzle"
+ * Return: (0)
  */
 
 int main(void)
@@ -14,10 +17,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli);
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli);
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
