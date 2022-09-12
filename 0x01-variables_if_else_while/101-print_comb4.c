87b6b357b059cdc5c73f10dc37f99d1b61a1789d
Dimeji267 <salawudimeji1@gmail>
2022-09-12 15:51:16 -0700

    Do what you do better

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,35 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Description: prints 3 combination of numbers
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int c, i, k;
+	int c, i, l;
 
 	for (c = '0' ; c <= '9' ; c++)
-{
-	for (i = '0' ; i <= '9' ; i++)
-{
-	for (k = '0' ; k <= '9' ; k++)
-{
-	if (c < i && i < k)
-{
-	putchar(c);
-	putchar(i);
-	putchar(k);
-}
-	if (c != '7')
-{
-	putchar(',');
-	putchar(' ');
-}
-}
-}
-}
+	{
+		for (i = '0' ; i <= '9' ; i++)
+		{
+			for (l = '0' ; l <= '9' ; l++)
+			{
+				if (c < i && i < l)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(l);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
