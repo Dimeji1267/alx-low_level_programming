919e203e22b196f847a7f5b64571e066590031e8
Dimeji267 <salawudimeji1@gmail>
2022-09-14 04:45:37 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,23 +2,24 @@
 /**
  * print_sign - function to check for a sign of a number
  * @n: is the int that will use for the argument of the function
- * Return: 0
+ * Return: 1 if +, 0 if 0, and -1 if -
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar('+');
-	return (1);
+		_putchar('+');
+		return (1);
 	}
-	else (n < 0);
+	else if (n < 0)
 	{
-	_putchar('-');
-	return (-1);
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
-	_putchar('0');
-	return (0);
+		_putchar('0');
+		return (0);
 	}
 }
