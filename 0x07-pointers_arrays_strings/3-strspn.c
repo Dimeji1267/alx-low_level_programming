32b07a7aa7bb8ed5b556b961b1f3db33c154441b
Dimeji267 <salawudimeji1@gmail>
2022-09-27 14:52:09 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,9 +2,9 @@
 /**
  * _strspn - gets the length of a prefix substring
  * @s: string
- * @accept: contains bytes that may or may compose parts of the string
+ * @accept: contains bytes that may or may not compose parts of the string
  *
- * Return: the number of bytes that compose length
+ * Return: the number of bytes that compose the length
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -18,7 +18,7 @@ unsigned int _strspn(char *s, char *accept)
 		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
 			;
 		if (s[i] == accept[j])
-			length;
+			length++;
 		if (accept[j] == '\0')
 			return (length);
 	}
