29032ef71af65e5dc3db533faecb376752edcdce
Dimeji267 <salawudimeji1@gmail>
2022-10-03 09:02:21 -0700

    Prodigy3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - multiples two numbers
- * @argc: number of arguments passed to the function
- * @argv: argument vector of pointers to strings
- *
- * Return: 0 if no errors, else 1
+ * main - multiplies two numbers
+ * @argc: number of command line arguments
+ * @argv: array that contains the progress command line arguments
+ * Return: 0 - success
  */
 int main(int argc, char *argv[])
 {
-	int a, b, c;
-
 	if (argc != 3)
 	{
-		puts("Error");
-		return (1);
+		printf("Error\n");
+		return (0);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	c = a * b;
-	printf("%d\n", c);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
