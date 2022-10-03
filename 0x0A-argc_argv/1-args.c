76a04569ffd833c0882807801e9c6485f2287239
Dimeji267 <salawudimeji1@gmail>
2022-10-03 08:26:19 -0700

    Prodigy1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints the name number of arguments passed as input
- * @argc: number of arguments passed at command
- * @argv: pointers to strings of arguments
- * Return: always 0.
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the progress command line arguments.
+ * Return: 0 - success
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
