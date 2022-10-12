5a63845ee8f421c70d50488fc7e0d5b66fd02e38
Dimeji267 <salawudimeji1@gmail>
2022-10-12 13:19:18 -0700

    Function1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,7 +3,7 @@
  * array_iterator - executes a func given as a param on each element of array
  * @array: array to execute func on
  * @size: size of array
- * @action: pointer to thr func to use
+ * @action: pointer to the func to use
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
